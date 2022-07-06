c95c657e96a9ce02adb7f8f1326174a16c604bbc
SaarBS <saar1.bs1@gmail.com>
2022-07-06 13:17:43 +0300

    Added reverse fibonachi

--- Fibonachi/Fibonachi.c
+++ Fibonachi/Fibonachi.c
@@ -3,8 +3,7 @@
 int Fibonachi(int location) {
 	int a = 1;
 	int b = 0;
-	for (int i = 0; i < location - 1; i++)
-	{
+	for (int i = 0; i < location - 1; i++){
 		int temp = a;
 		a = a + b;
 		b = temp;
@@ -12,10 +11,28 @@ int Fibonachi(int location) {
 	return a;
 }
 
+int ReverseFibonachi(int number) {
+	int location = 1;
+	int a = 1;
+	int b = 0;
+	while (a < number){
+		int temp = a;
+		a = a + b;
+		b = temp;
+		location++;
+	}
+	if (a == number) {
+		return location;
+	} else {
+		return -1;
+	}
+}
+
 int main()
 {
 	for (int i = 1; i < 11; i++)
 	{
-		printf("%d, ", Fibonachi(i));
+		int fibonachi = Fibonachi(i);
+		printf("(%d, %d), ", ReverseFibonachi(fibonachi), fibonachi);
 	}
 }
