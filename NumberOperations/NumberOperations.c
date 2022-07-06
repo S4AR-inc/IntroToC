e1a8b969c93927eb4cb2907b06177343521a2a7c
SaarBS <saar1.bs1@gmail.com>
2022-07-06 15:31:04 +0300

    Add by pointer function

--- NumberOperations/NumberOperations.c
+++ NumberOperations/NumberOperations.c
@@ -8,10 +8,19 @@ int sqrt(int num) {
     return root - 1;
 }
 
+void add(int* pNum, int incBy) {
+    *pNum += incBy;
+}
+
 int main()
 {
     for (int i = 1; i < 100; i++)
 	{
         printf("root of %d is %d\n", i, sqrt(i));
 	}
+
+    int number = 6;
+    printf("%d + 60 = ", number);
+    add(&number, 60);
+    printf("%d\n", number);
 }
