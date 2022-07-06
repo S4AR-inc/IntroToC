66a9955097045765521e1117279cd159162bd92f
SaarBS <saar1.bs1@gmail.com>
2022-07-06 16:11:02 +0300

    Created a function to fill array with fibonachi

--- Fibonachi/Fibonachi.c
+++ Fibonachi/Fibonachi.c
@@ -28,6 +28,19 @@ int ReverseFibonachi(int number) {
 	}
 }
 
+void FillFibonachi(int* array, int length) {
+	int a = 1;
+	int b = 0;
+	for (int i = 0; i < length; i++){
+		array[i] = a;
+		int temp = a;
+		a = a + b;
+		b = temp;
+	}
+	return a;
+
+}
+
 int main()
 {
 	for (int i = 1; i < 11; i++)
@@ -35,4 +48,12 @@ int main()
 		int fibonachi = Fibonachi(i);
 		printf("(%d, %d), ", ReverseFibonachi(fibonachi), fibonachi);
 	}
+	printf("\n");
+	int fibo[10];
+	FillFibonachi(&fibo, 10);
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d, ", fibo[i]);
+	}
+
 }
